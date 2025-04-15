2ccdeb5adebe0fef45a84cc5ffd377b884323a95
wixette <wixette@gmail.com>
2025-04-15 19:48:31 +0800

    Add comments and spaces logic to parser

--- src/fountain_parser.js
+++ src/fountain_parser.js
@@ -1,5 +1,37 @@
-function parse(/* fountainText */) {
-  return {};
+const REGEX = {
+  BONEYARD: /\/\*.*?\*\//gms,
+  EXTRA_LINE_BREAKS: /^\n+|\n+$/,
+  EXTRA_WHITESPACES: /^\t+|^ {3,}/gm,
+  LINE_BREAKS: /\r\n|\r/g,
+  PAGE_BREAK: /^={3,}$/,
+  SPLITTER: /\n{2,}/g,
+  TWO_SPACES_LINE_BREAK: /^ {2}$/,
+};
+
+function preprocess(fountainText) {
+  // 1. Removes boneyards (comments).
+  // 2. Normalizes line breaks.
+  // 3. Clears extra line breaks.
+  // 4. Clears extra whitespaces.
+  return fountainText.replace(REGEX.BONEYARD, '')
+    .replace(REGEX.LINE_BREAKS, '\n')
+    .replace(REGEX.EXTRA_LINE_BREAKS, '')
+    .replace(REGEX.EXTRA_WHITESPACES, '');
+}
+
+function parse(fountainText) {
+  const text = preprocess(fountainText);
+  const lines = text.split(REGEX.SPLITTER);
+  const blockObjects = [];
+  for (const line of lines) {
+    const blockObject = {};
+    blockObject.text = line;
+    blockObjects.push(blockObject);
+  }
+  return {
+    blocks: blockObjects,
+    metadata: {},
+  };
 }
 
 export {
--- src/fountain_parser.test.js
+++ src/fountain_parser.test.js
@@ -2,6 +2,24 @@ import assert from 'node:assert'
 import test from 'node:test';
 import { parse } from './fountain_parser.js';
 
-test('parser basic cases', () => {
-  assert.strictEqual(JSON.stringify(parse({}, 'default')), '{}');
+const TEXT_1 = `/*
+Line break and boneyard test
+*/
+
+/* Two-space line break */
+
+Action line 1.
+  /* Extra spaces */
+additional text.
+
+
+Action line 2.
+`
+
+test('parser: basic cases', () => {
+  let result = parse('');
+  assert.strictEqual(result.blocks.length, 1);
+
+  result = parse(TEXT_1);
+  assert.strictEqual(result.blocks.length, 2);
 });
